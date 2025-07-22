b0dde172d308d360997467b1ace0a2bb3f238590
kritikaasachdeva <kritika1962.be23@chitkara.edu.in>
2025-07-22 09:30:45 +0530

    Add files via upload

---
+++ projectSession2/projectSession2Tests/projectSession2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  projectSession2Tests.swift
+//  projectSession2Tests
+//
+//  Created by Student on 19/07/25.
+//
+
+import Testing
+@testable import projectSession2
+
+struct projectSession2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
